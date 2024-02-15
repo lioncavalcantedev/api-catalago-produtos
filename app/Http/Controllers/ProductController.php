9b307e081805b1bb90f181647e0fc27562a8eedb
Lion Cavalcante <lioncavalcantedev@gmail.com>
2024-02-15 01:06:33 -0300

    Adicionar controller, model, rotas e migrations ao projeto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['error' => 'Produto não encontrado.'], 404);
+        }
+
+        return response()->json($product);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+    
+        $product = Product::create($request->all());
+    
+        return response()->json($product , 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['error' => 'Produto não encontrado.'], 404);
+        }
+
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['error' => 'Produto não encontrado.'], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(['message' => 'Produto removido com sucesso.']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,11 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/products', [ProductController::class, 'listProducts']);
-Route::get('/products', [ProductController::class, 'getProduct']);
-Route::get('/products', [ProductController::class, 'createProduct']);
-Route::get('/products', [ProductController::class, 'updateProduct']);
-Route::get('/products', [ProductController::class, 'deleteProduct']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
